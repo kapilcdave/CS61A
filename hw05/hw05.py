1277d1304b0e0d85b93d250f4973892df9a09752
kapilcdave <kapilmcdave@gmail.com>
2026-03-12 13:37:44 -0700

    donehw5

--- hw05/hw05.py
+++ hw05/hw05.py
@@ -9,7 +9,14 @@ def hailstone(n):
     >>> next(hail_gen)
     1
     """
-    "*** YOUR CODE HERE ***"
+    while n > 1:
+        yield n
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+    while True:
+        yield 1
 
 
 def merge(a, b):
@@ -30,11 +37,14 @@ def merge(a, b):
     a_val, b_val = next(a), next(b)
     while True:
         if a_val == b_val:
-            "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val, b_val = next(a), next(b)
         elif a_val < b_val:
-            "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val = next(a)
         else:
-            "*** YOUR CODE HERE ***"
+            yield b_val
+            b_val = next(b)
 
 
 def stair_ways(n):
@@ -50,7 +60,15 @@ def stair_ways(n):
     >>> list(s_w) # Ensure you're not yielding extra
     []
     """
-    "*** YOUR CODE HERE ***"
+    if n == 0:
+        yield []
+    elif n == 1:
+        yield [1]
+    else:
+        for way in stair_ways(n-1):
+            yield [1] + way
+        for way in stair_ways(n-2):
+            yield [2] + way
 
 
 def yield_paths(t, target):
@@ -89,10 +107,10 @@ def yield_paths(t, target):
     [[0, 2], [0, 2, 1, 2]]
     """
     if label(t) == target:
-        yield ____
+        yield [label(t)]
     for b in branches(t):
-        for ____ in ____:
-            yield ____
+        for path in yield_paths(b, target):
+            yield [label(t)] + path
 
 
 
