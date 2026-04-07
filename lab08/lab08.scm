50295f5bbba3065834c4bd708a570e177c09128a
kapilcdave <kapilmcdave@gmail.com>
2026-04-07 16:05:12 -0700

    lab8done

--- lab08/lab08.scm
+++ lab08/lab08.scm
@@ -1,8 +1,23 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (if (< num1 num2)
+      -1
+      (if (= num1 num2)
+          0
+          1)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (over-or-under-cond num1 num2)
+  (cond ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        (else 1)))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (composed f g)
+  (lambda (x)
+    (f (g x))))
+
+(define (repeat f n)
+  (if (= n 1)
+      (lambda (x) (f x))
+      (composed f (repeat f (- n 1)))))
 
 (define (max a b)
   (if (> a b)
@@ -14,12 +29,31 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+  (if (zero? (min a b))
+      (max a b)
+      (if (zero? (modulo (max a b) (min a b)))
+          (min a b)
+          (gcd (min a b)
+               (modulo (max a b) (min a b))))))
 
 (define (exp b n)
-  (define (helper n so-far) "YOUR CODE HERE")
+  (define (helper n so-far)
+    (if (= n 0)
+        so-far
+        (helper (- n 1) (* so-far b))))
   (helper n 1))
 
-(define (swap s) 'YOUR-CODE-HERE)
+(define (swap s)
+  (define (swap-helper s result)
+    (cond ((null? s) result)                   
+          ((null? (cdr s)) (append result s))  
+          (else 
+            (swap-helper (cdr (cdr s)) 
+                         (append result (list (car (cdr s)) (car s)))))))
+  (swap-helper s nil))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+
+(define (make-adder num)
+  (lambda (x)
+    (+ x num)))
