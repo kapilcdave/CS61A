04085e6baf41036c4352e1f0a6551c751992ad35
kapilcdave <kapilmcdave@gmail.com>
2026-04-16 00:29:07 -0700

    done hw scheme next

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,7 +1,18 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (or (null? s)
+      (null? (cdr s))
+      (and (<= (car s) (car (cdr s)))
+           (ascending? (cdr s)))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (cond ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (cond ((null? lst1) lst2)
+        (else (cons (car lst1) (interleave lst2 (cdr lst1))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+  (cond ((null? s) '())
+        (else (cons (car s) (no-repeats (my-filter (lambda (x) (not (= x (car s)))) (cdr s)))))))
